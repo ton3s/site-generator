4d80ee61a64171b7db615b7277bb93f49bf07cb8
ton3s <antonio.cunanan@gmail.com>
2025-05-24 10:48:42 -0500

    Add LeafNode class and unit tests for to_html method

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -10,7 +10,7 @@ class HTMLNode:
 		raise NotImplementedError("to_html not implemented")
 	
 	def props_to_html(self):
-		html = ""
+		html = ""		
 		for key, value in self.props.items():
 			html += f" {key}=\"{value}\""
 		return html
@@ -18,3 +18,18 @@ class HTMLNode:
 	def __repr__(self):
 		return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
 	
+class LeafNode(HTMLNode):
+	def __init__(self, tag, value, props = None):
+		super().__init__(tag, value, children = None, props = props)
+	
+	def to_html(self):
+		if self.value is None:
+			raise ValueError("Value cannot be None for a leaf node")
+		if self.tag is None:
+			return self.value
+		if self.props is None:
+			return f"<{self.tag}>{self.value}</{self.tag}>"
+		return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+	
+	def __repr__(self):
+		return f"LeafNode({self.tag}, {self.value}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):	
 	def test_props_to_html_empty(self):
@@ -14,3 +14,14 @@ class TestHTMLNode(unittest.TestCase):
 		node = HTMLNode(props={"class": "test", "id": "test"})
 		self.assertEqual(node.props_to_html(), " class=\"test\" id=\"test\"")
 	
+	def test_leaf_to_html_p(self):
+		node = LeafNode(tag="p", value="Hello, world!")
+		self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+	
+	def test_leaf_to_html_p_with_props(self):
+		node = LeafNode(tag="p", value="Hello, world!", props={"class": "test", "id": "test"})
+		self.assertEqual(node.to_html(), "<p class=\"test\" id=\"test\">Hello, world!</p>")
+	
+	def test_leaf_to_html_p_with_no_tag(self):
+		node = LeafNode(tag=None, value="Hello, world!")
+		self.assertEqual(node.to_html(), "Hello, world!")
