30334917931ef69cba01417751d6201fa2b0d7ae
ton3s <antonio.cunanan@gmail.com>
2025-05-24 11:17:32 -0500

    Add ParentNode class with to_html method and unit tests for child handling

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,4 +1,3 @@
-
 class HTMLNode:
 	def __init__(self, tag = None, value = None, children = None, props = None):
 		self.tag = tag
@@ -34,3 +33,21 @@ class LeafNode(HTMLNode):
 	
 	def __repr__(self):
 		return f"LeafNode({self.tag}, {self.value}, {self.props})"
+	
+class ParentNode(HTMLNode):
+	def __init__(self, tag, children, props = None):
+		super().__init__(tag, children = children, props = props)
+	
+	def to_html(self):
+		if self.tag is None:
+			raise ValueError("Tag cannot be None for a parent node")
+		if self.children is None:
+			raise ValueError("Children cannot be None for a parent node")
+		children_html = ""
+		for child in self.children:
+			children_html += child.to_html()
+		final_html = f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+		return final_html
+	
+	def __repr__(self):
+		return f"ParentNode({self.tag}, {self.children}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 class TestHTMLNode(unittest.TestCase):	
 	def test_props_to_html_empty(self):
@@ -25,3 +25,17 @@ class TestHTMLNode(unittest.TestCase):
 	def test_leaf_to_html_p_with_no_tag(self):
 		node = LeafNode(tag=None, value="Hello, world!")
 		self.assertEqual(node.to_html(), "Hello, world!")
+
+	def test_to_html_with_children(self):
+		child_node = LeafNode("span", "child")
+		parent_node = ParentNode("div", [child_node])
+		self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+	def test_to_html_with_grandchildren(self):
+		grandchild_node = LeafNode("b", "grandchild")
+		child_node = ParentNode("span", [grandchild_node])
+		parent_node = ParentNode("div", [child_node])
+		self.assertEqual(
+			parent_node.to_html(),
+			"<div><span><b>grandchild</b></span></div>",
+		)
