445ade1587a494806fe2306652932a438f11da09
ton3s <antonio.cunanan@gmail.com>
2025-05-24 11:04:39 -0500

    Refactor props_to_html method in HTMLNode class to handle None props and improve HTML attribute generation

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -10,10 +10,13 @@ class HTMLNode:
 		raise NotImplementedError("to_html not implemented")
 	
 	def props_to_html(self):
-		html = ""		
-		for key, value in self.props.items():
-			html += f" {key}=\"{value}\""
-		return html
+		if self.props is None:
+			return ""
+		props_html = ""
+		for prop in self.props:
+			props_html += f' {prop}="{self.props[prop]}"'
+		return props_html
+
 	
 	def __repr__(self):
 		return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
@@ -27,8 +30,6 @@ class LeafNode(HTMLNode):
 			raise ValueError("Value cannot be None for a leaf node")
 		if self.tag is None:
 			return self.value
-		if self.props is None:
-			return f"<{self.tag}>{self.value}</{self.tag}>"
 		return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
 	
 	def __repr__(self):
